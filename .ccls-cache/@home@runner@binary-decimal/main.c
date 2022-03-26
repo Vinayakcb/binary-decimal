2d68108edd416abcc6ce11e3296b62ee6eae6e0d
Vinayak C C <102400867+Vinayakcb@users.noreply.github.com>
2022-03-26 04:45:36 +0000

    hi

--- .ccls-cache/@home@runner@binary-decimal/main.c
+++ .ccls-cache/@home@runner@binary-decimal/main.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-int btod(int); // Prototype of btod() function
+int btod(int); 
 int main()
 {
 int binary,decimal;
-// Accept input in binary format
+
 printf("Enter binary input :");
 scanf("%d",&binary);
-// Invoke btod() to convert binary to decimal
+
 decimal=btod(binary);
-// Print the decimal equivalent of binary
+
 printf("Decimal equivalent = %d\n", decimal);
 return 0;
 }
-// Function Definition of btod() function
+
 int btod(int bin)
 {
 if (bin==0)
